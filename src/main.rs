b866bc673a7414d78f552353aadfd80f3ffc8bb4
Mukundi Chitamba <mukundichitamba@Mukundis-MacBook-Pro.local>
2026-02-03 22:25:07 +0200

    rust practice

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::f32::consts::PI;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
-use sdl2::rect::{FRect, Rect};
+use sdl2::rect::{FPoint, FRect, Rect};
 use sdl2::render::{Canvas, WindowCanvas};
 use log::debug;
 
@@ -14,15 +15,24 @@ pub fn main() {
     let video_subsystem = sdl_context.video().unwrap();
 
     let points: Vec<P> = vec![
-        P { x: 0.5, y: 0.5, z: 0.5},
-        P { x: -0.5, y: 0.5, z: 0.5},
-        P { x: 0.5, y: -0.5, z: 0.5},
-        P { x: -0.5, y: -0.5, z: 0.5},
+        P { x: 0.25, y: 0.25, z: 0.25},
+        P { x: -0.25, y: 0.25, z: 0.25},
+        P { x: -0.25, y: -0.25, z: 0.25},
+        P { x: 0.25, y: -0.25, z: 0.25},
 
-        P { x: 0.5, y: 0.5, z: -0.5},
-        P { x: -0.5, y: 0.5, z: -0.5},
-        P { x: 0.5, y: -0.5, z: -0.5},
-        P { x: -0.5, y: -0.5, z: -0.5},
+        P { x: 0.25, y: 0.25, z: -0.25},
+        P { x: -0.25, y: 0.25, z: -0.25},
+        P { x: -0.25, y: -0.25, z: -0.25},
+        P { x: 0.25, y: -0.25, z: -0.25},
+    ];
+
+    let faces: Vec<Vec<i32>> = vec![
+        vec![0, 1, 2, 3],
+        vec![4, 5, 6, 7],
+        vec![0, 4],
+        vec![1, 5],
+        vec![2, 6],
+        vec![3, 7],
     ];
 
     let window = video_subsystem.window("rust-sdl2 demo", WIDTH as u32, HEIGHT as u32)
@@ -33,9 +43,11 @@ pub fn main() {
     let mut canvas = window.into_canvas().build().unwrap();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut z = 0.0;
+    let mut z = 1.0;
+    let mut angle = 1.0;
     'running: loop {
-        z = z + 0.01;
+        // z = z + 0.01;
+        angle = angle + PI * (1.0/60.0);
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -48,8 +60,20 @@ pub fn main() {
 
         clear(&mut canvas);
 
-        for p in &points {
-            point(&mut canvas, screen(project(translate(p.clone(), z))));
+        // for p in &points {
+        //     point(&mut canvas, screen(project(translate(rotate_xz(p.clone(), angle), z))));
+        // }
+
+        for face in &faces {
+            for (index, _) in face.iter().enumerate() {
+                let p1 = points[face[index] as usize];
+                let p2 = points[face[(index+1)%face.len()] as usize];
+                line(
+                    &mut canvas,
+                    screen(project(translate(rotate_xz(p1.clone(), angle), z))),
+                    screen(project(translate(rotate_xz(p2.clone(), angle), z)))
+                );
+            }
         }
 
         canvas.present();
@@ -67,6 +91,12 @@ pub fn point (canvas: &mut WindowCanvas, p: P) {
     canvas.set_draw_color(Color::RGB(255, 210, 0));
     let fill_rect = FRect::new(p.x - POINT_SIZE/2.0, p.y - POINT_SIZE/2.0, POINT_SIZE, POINT_SIZE);
     canvas.fill_frect(fill_rect).unwrap();
+}
+
+pub fn line (canvas: &mut WindowCanvas, p1: P, p2: P) {
+    canvas.set_draw_color(Color::RGB(255, 210, 0));
+    let p = FPoint::new(p1.x, p1.y);
+    canvas.draw_fline(FPoint::new(p1.x, p1.y), FPoint::new(p2.x, p2.y));
 }
 
 pub fn screen(p: P) -> P {
@@ -93,11 +123,11 @@ pub fn translate(p: P, dz: f32) -> P {
     }
 }
 
-pub fn rotate_x(p: P, angle: f32) -> P {
+pub fn rotate_xz(p: P, angle: f32) -> P {
     P {
-        x: p.x * angle.cos() - p.y * angle.sin(),
-        y: p.x * angle.sin() + p.y * angle.cos(),
-        z: p.z
+        x: p.x * angle.cos() - p.z * angle.sin(),
+        z: p.x * angle.sin() + p.z * angle.cos(),
+        y: p.y
     }
 }
 
